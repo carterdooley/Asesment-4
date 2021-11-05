5c3e4aa84b77600a5c6c9410c29ef04c1ee5a118
carter dooley <carter.j.dooley43@gmail.com>
2021-11-05 15:16:44 -0500

    Finished First Step

--- server/index.js
+++ server/index.js
@@ -22,4 +22,20 @@ app.get("/api/compliment", (req, res) => {
   
 });
 
+app.get("/api/fortune", (req, res) => {
+  const fortunes = ["You will win the lottery",
+          "Great food will fall from the sky",
+        "Your house will disapear next time you leave",
+        "A good time to finish up old tasks", 
+        "Advice, when most needed, is least heeded."]
+
+
+
+  let randoIndex = Math.floor(Math.random() * fortunes.length)
+  let randoFort = fortunes[randoIndex]
+
+  res.status(200).send(randoFort)
+})
+
+
 app.listen(4000, () => console.log("Server running on 4000"));
